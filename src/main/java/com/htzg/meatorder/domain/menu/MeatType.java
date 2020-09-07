4bff55df18a1a7320ae0b6dd27285e96148f84f9
jbyjby1 <178673923@qq.com>
2020-09-07 13:44:20 +0800

    更新最新菜单，增加餐的类型（凉皮、炒饭）

--- src/main/java/com/htzg/meatorder/domain/menu/MeatType.java
+++ src/main/java/com/htzg/meatorder/domain/menu/MeatType.java
@@ -2,7 +2,7 @@ package com.htzg.meatorder.domain.menu;
 
 public enum MeatType {
 
-    PILAFF("盖浇饭"), FRY("炒菜"), NOODLE("面食"),
+    PILAFF("盖浇饭"), FRY("炒菜"), NOODLE("面食"), FRIED_RICE("炒饭"), GRIDDLE("锅类"),
     RICE_NOODLE("米粉"), PICKLE("小菜"), DRINK("饮料"),
     CHINESE_HAMBURGER("肉夹馍"), PORRIDGE("汤粥类"), RICE("米饭"),
     FAST_FOOD("快餐");
